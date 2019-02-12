b0263ac686ab91effbd1e553f0ba1d205dda8547
Dennis Gilli <dennis.gilli@acribis.ch>
2019-02-12 17:04:29 +0100

    Add translation files

---
+++ translations/de/site.php
@@ -0,0 +1,88 @@
+<?php
+return [
+
+    // #
+    // A
+
+    "A member of the" => "Ein Mitglied der",
+
+
+    // B
+    // C
+
+    "Copyright Notice" => "Copyright Hinweis",
+
+
+    // D
+    // E
+
+    "Email" => "E-Mail",
+    "Example: © 2017, {Notice}" => "Beispiel: © 2017, {Hinweis}",
+
+
+    // F
+    // G
+    // H
+    // I
+
+    "ISO Notice" => "ISO Hinweis",
+
+
+    // J
+    // K
+    // L
+    // M
+    // N
+    // O
+    // P
+
+    "Phone" => "Tel.",
+
+
+    // Q
+    // R
+    // S
+    // T
+    // U
+    // V
+    // W
+    // X
+    // Y
+    // Z
+
+    //
+    // Deutsche Übersetzungen für statische Texte
+    // --------------------------------------------------
+    //== Produkte
+    //
+    //##
+    "tlSpecification" => "Spezifikationen",
+    //== News
+    //
+    //##
+    "tlCurrentNews" => "Aktuell",
+    "tlOlderNews" => "Ältere Artikel",
+    "tlBack" => "< Zurück",
+    //== Jobs
+    //
+    //##
+    "tlNoOpenings" => "Derzeit sind keine offene Stellen verhanden.",
+    //== Kontakt
+    //
+    //##
+    "tlAddress" => "Adresse",
+    "tlAddressPhone" => "Tel.",
+    "tlAddressEmail" => "E-Mail",
+    "tlAddressWebsite" => "Webseite",
+    "tlName" => "Name",
+    "tlEmail" => "E-Mail",
+    "tlSubject" => "Betreff",
+    "tlMessage" => "Nachricht",
+    "tlSend" => "Senden",
+    "tlSendSuccess" => "Nachricht wurde Erfolgreich gesendet!",
+    //== 404 - Seite nicht gefunden
+    //
+    //##
+    "tlPageNotFound" => "Seite nicht gefunden",
+    "tlBackToHome" => "Zurück zur Startseite",
+];
---
+++ translations/es/site.php
@@ -0,0 +1,38 @@
+<?php
+return array(
+    //
+    // Deutsche Übersetzungen für statische Texte
+    // --------------------------------------------------
+    //== Produkte
+    //
+    //##
+    "tlSpecification" => "Spezifikationen",
+    //== News
+    //
+    //##
+    "tlCurrentNews" => "Aktuell",
+    "tlOlderNews" => "Ältere Artikel",
+    "tlBack" => "< Zurück",
+    //== Jobs
+    //
+    //##
+    "tlNoOpenings" => "Derzeit sind keine offene Stellen verhanden.",
+    //== Kontakt
+    //
+    //##
+    "tlAddress" => "Adresse",
+    "tlAddressPhone" => "Tel.",
+    "tlAddressEmail" => "E-Mail",
+    "tlAddressWebsite" => "Webseite",
+    "tlName" => "Name",
+    "tlEmail" => "E-Mail",
+    "tlSubject" => "Betreff",
+    "tlMessage" => "Nachricht",
+    "tlSend" => "Senden",
+    "tlSendSuccess" => "Nachricht wurde Erfolgreich gesendet!",
+    //== 404 - Seite nicht gefunden
+    //
+    //##
+    "tlPageNotFound" => "Seite nicht gefunden",
+    "tlBackToHome" => "Zurück zur Startseite",
+);

faf58834b1ad897577b0a728dcc95ce0db2604b9
Dennis Gilli <dennis.gilli@acribis.ch>
2019-02-12 17:04:52 +0100

    Add image dependencies

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,16 @@
-import styles from '../css/app.scss';
+import '../css/app.scss';
+
+// Require logos
+import '../img/christundheiri_logo_en.png';
+import '../img/christundheiri_logo_de.png';
+import '../img/christundheiri_logo_fr.png';
+import '../img/grieshaber_logo_en.png';
+import '../img/grieshaber_logo_de.png';
+import '../img/grieshaber_logo_es.png';
+
+// Require iso image
+import '../img/christundheiri_footer_iso.png';
+import '../img/grieshaber_footer_iso.png';
 
 // App main
 const main = async () => {
